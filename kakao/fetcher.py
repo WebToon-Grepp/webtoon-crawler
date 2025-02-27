e401f5bce285790bee9708a6af2416a3dd7d3504
Rooam Lee <leewr9@naver.com>
2025-02-27 14:48:58 +0900

    kakao:: titles 파일명 수정

--- kakao/fetcher.py
+++ kakao/fetcher.py
@@ -62,7 +62,7 @@ def fetch_titles_from_url(url, subfolder_name, file_name):
 
 def fetch_titles(day):
     url = KakaoWebtoonEndpoint.TITLES.value[day]
-    return fetch_titles_from_url(url, "titles", "titles")
+    return fetch_titles_from_url(url, "titles", f"titles_{day}")
 
 
 def fetch_finished_titles():
@@ -143,6 +143,6 @@ def fetch_daily_data(day):
 # Main Call Function
 def fetch_all_data():
     clear_folder()
-    
+
     fetch_daily_data(0)
 
