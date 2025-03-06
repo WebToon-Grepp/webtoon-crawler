1cacbbcffa07ae24b0cd3eb153c864f06d322546
Rooam Lee <leewr9@naver.com>
2025-03-06 13:10:29 +0900

    crawler:: spark 최적화 과정 간소화

--- kakao/optimizer.py
+++ kakao/optimizer.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import (
-    col, lit, when, explode, concat_ws, split, 
-    to_date, from_utc_timestamp, input_file_name
+    col, explode, split, input_file_name
 )
 from datetime import datetime
 
@@ -15,101 +14,44 @@ RAW = "s3a://{bucket}/raw/{platform}/{target}/{target_date}"
 def get_comments(spark, date):
     url = RAW.format(bucket=BUCKET, platform=PLATFORM, target="comments", target_date=date)
     df = spark.read.json(f"{url}/*/*.json", multiLine=True)
-    df = df.withColumn("filename", input_file_name()) \
-           .withColumn("title_id", split(col("filename"), "/").getItem(9)) \
-           .withColumn("episode_id", split(col("filename"), "/").getItem(10))
-    return df.select(
-            lit(PLATFORM).alias("platform"),
-            col("title_id"),
-            split(col("episode_id"), "\.").getItem(0).alias("id"),
-            col("meta.pagination.totalCount").alias("comments")
-        )
+    return df.withColumn("filename", input_file_name()) \
+             .withColumn("title_id", split(col("filename"), "/").getItem(9)) \
+             .withColumn("episode_id", split(col("filename"), "/").getItem(10))
 
 
 def get_episode_likes(spark, date):
     url = RAW.format(bucket=BUCKET, platform=PLATFORM, target="episode_likes", target_date=date)
     df = spark.read.json(f"{url}/*/*.json", multiLine=True)
-    df = df.withColumn("filename", input_file_name())
-    return df.select(
-            lit(PLATFORM).alias("platform"),
-            split(col("filename"), "/").getItem(9).alias("title_id"),
-            col("data.episodeId").alias("id"),
-            col("data.likeCount").alias("likes")
-        )
+    return df.withColumn("filename", input_file_name())
 
 
 def get_episodes(spark, date):
     url = RAW.format(bucket=BUCKET, platform=PLATFORM, target="episodes", target_date=date)
     df = spark.read.json(f"{url}/*/*.json", multiLine=True)
-    return df.select(explode(col("data.episodes")).alias("episodes")) \
-        .select(
-            lit(PLATFORM).alias("platform"),
-            col("episodes.contentId").alias("title_id"),
-            col("episodes.id").alias("id"),
-            col("episodes.title").alias("title"),
-            col("episodes.asset.thumbnailImage").alias("image_url"),
-            col("episodes.useStartDateTime").alias("start_date")
-        )
+    return df.select(explode(col("data.episodes")).alias("episodes"))
 
 
 def get_title_info(spark, date):
     url = RAW.format(bucket=BUCKET, platform=PLATFORM, target="title_info", target_date=date)
-    df = spark.read.json(f"{url}/*.json", multiLine=True)
-    return df.select(
-            lit(PLATFORM).alias("platform"),
-            col("data.id").alias("id"),
-            col("data.statistics.viewCount").alias("views")
-        )
+    return spark.read.json(f"{url}/*.json", multiLine=True)
 
 
 def get_titles(spark, date):
     url = RAW.format(bucket=BUCKET, platform=PLATFORM, target="titles", target_date=date)
     df = spark.read.json(f"{url}/*.json", multiLine=True)
-    return df.select(explode(col("data")).alias("data")) \
-        .select(
-            explode(col("data.cardGroups")).alias("cardGroups"), 
-            col("data.title").alias("weekday_str")
-        ).select(explode(col("cardGroups.cards")).alias("cards"), col("weekday_str")) \
-        .select(col("cards.content").alias("content"), col("weekday_str")) \
-        .select(
-            lit(PLATFORM).alias("platform"),
-            col("content.id").alias("id"), 
-            col("content.title").alias("title"), 
-            concat_ws(" / ", col("content.authors.name")).alias("author"),
-            col("content.featuredCharacterImageA").alias("image_url"), 
-            col("weekday_str"),
-            lit(False).alias("is_completed"),
-            explode("content.seoKeywords").alias("genre_name")
-        )
+    return df.select(explode(col("data")).alias("data"))
 
 
 def save_to_parquet(df, target):
     date_str = NOW.strftime("year=%Y/month=%m/day=%d")
-    path = f"s3a://wt-grepp-lake/optimized/{target}/{date_str}"
-    df.coalesce(50).write.partitionBy("platform").format("parquet").mode("append").save(path)
+    path = f"s3a://wt-grepp-lake/optimized/{target}/{date_str}/platform={PLATFORM}"
+    df.coalesce(50).write.format("parquet").mode("append").save(path)
     print(f"Data successfully optimized to {path}")
-
-
-def convert_weekday(df):
-    return df.withColumn("release_day", 
-                         when(col("weekday_str") == "월", 0)
-                         .when(col("weekday_str") == "화", 1)
-                         .when(col("weekday_str") == "수", 2)
-                         .when(col("weekday_str") == "목", 3)
-                         .when(col("weekday_str") == "금", 4)
-                         .when(col("weekday_str") == "토", 5)
-                         .when(col("weekday_str") == "일", 6)
-                         .otherwise(7)) 
-
-
-def convert_timestamp(df):
-    return df.withColumn("timestamp", from_utc_timestamp(col("start_date"), "UTC")) \
-             .withColumn("updated_date", to_date(col("timestamp")))
 
 
 def create_spark_session():
     return SparkSession.builder \
-        .appName("S3 Data Optimizer") \
+        .appName(f"S3 {PLATFORM} Data Optimizer") \
         .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
         .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
@@ -121,19 +63,18 @@ def run():
     date_str = NOW.strftime("%Y/%m/%d")
     
     titles_df = get_titles(spark, date_str)
-    titles_df = convert_weekday(titles_df)
-    title_info_df = get_title_info(spark, date_str)
-
-    episodes_df = get_episodes(spark, date_str)
-    episodes_df = convert_timestamp(episodes_df)
-    episode_likes_df = get_episode_likes(spark, date_str)
-    comments_df = get_comments(spark, date_str)
-
     save_to_parquet(titles_df, "titles")
+
+    title_info_df = get_title_info(spark, date_str)
     save_to_parquet(title_info_df, "title_info")
 
+    episodes_df = get_episodes(spark, date_str)
     save_to_parquet(episodes_df, "episodes")
+
+    episode_likes_df = get_episode_likes(spark, date_str)
     save_to_parquet(episode_likes_df, "episode_likes")
+
+    comments_df = get_comments(spark, date_str)
     save_to_parquet(comments_df, "comments")
 
 
--- naver/optimizer.py
+++ naver/optimizer.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import (
-    col, lit, when, explode, split, 
-    to_date, from_unixtime
+    col, lit, explode
 )
 from datetime import datetime
 
@@ -14,35 +13,12 @@ RAW = "s3a://{bucket}/raw/{platform}/{target}/{target_date}"
 
 def get_comments(spark, date):
     url = RAW.format(bucket=BUCKET, platform=PLATFORM, target="comments", target_date=date)
-    df = spark.read.json(f"{url}/*/*.json", multiLine=True)
-    return df.select(
-            col("result.commentList").getItem(0).alias("comment"), 
-            col("result.count.total").alias("comments")
-        ).select(
-            lit(PLATFORM).alias("platform"),
-            split(col("comment.objectId"), "_").getItem(0).alias("title_id"),
-            split(col("comment.objectId"), "_").getItem(1).alias("id"),
-            col("comments")
-        )
+    return spark.read.json(f"{url}/*/*.json", multiLine=True)
 
 
 def get_episode_likes(spark, date):
     url = RAW.format(bucket=BUCKET, platform=PLATFORM, target="episode_likes", target_date=date)
-    df = spark.read.json(f"{url}/*/*.json", multiLine=True)
-    return df.select(
-            col("contents").getItem(0).alias("content"),
-            col("timestamp").alias("start_date")
-        ).select(
-            explode(col("content.reactions")).alias("reaction"),
-            col("content.contentsId").alias("contentsId"),
-            col("start_date")
-        ).select(
-            lit(PLATFORM).alias("platform"),
-            split(col("contentsId"), "_").getItem(0).alias("title_id"),
-            split(col("contentsId"), "_").getItem(1).alias("id"),
-            col("reaction.count").alias("likes"),
-            col("start_date")
-        )
+    return spark.read.json(f"{url}/*/*.json", multiLine=True)
 
 
 def get_episodes(spark, date):
@@ -51,23 +27,12 @@ def get_episodes(spark, date):
     return df.select(
             explode(col("articleList")).alias("article"), 
             col("titleId").alias("title_id")
-        ).select(
-            lit(PLATFORM).alias("platform"),
-            col("title_id"),
-            col("article.no").alias("id"),
-            col("article.subtitle").alias("title"),
-            col("article.thumbnailUrl").alias("image_url")
         )
 
 
 def get_title_info(spark, date):
     url = RAW.format(bucket=BUCKET, platform=PLATFORM, target="title_info", target_date=date)
-    df = spark.read.json(f"{url}/*.json", multiLine=True)
-    return df.select(
-            lit(PLATFORM).alias("platform"),
-            col("gfpAdCustomParam.titleId").alias("title_id"),
-            explode("gfpAdCustomParam.tags").alias("genre_name")
-        )
+    return spark.read.json(f"{url}/*.json", multiLine=True)
 
 
 def get_titles(spark, date, dayInt):
@@ -75,46 +40,22 @@ def get_titles(spark, date, dayInt):
 
     url = RAW.format(bucket=BUCKET, platform=PLATFORM, target="titles", target_date=date)
     df = spark.read.json(f"{url}/*.json", multiLine=True)
-    return df.select(explode(col(f"titleListMap.{days[dayInt]}")).alias("title")) \
-        .select(
-            lit(PLATFORM).alias("platform"),
-            col("title.titleId").alias("id"), 
-            col("title.titleName").alias("title"), 
-            col("title.author").alias("author"),
-            col("title.viewCount").alias("views"),
-            col("title.thumbnailUrl").alias("image_url"), 
-            lit(days[dayInt]).alias("weekday_str"),
-            lit(False).alias("is_completed")
-        )
+    return df.select(
+        explode(col(f"titleListMap.{days[dayInt]}")).alias("title"),
+        lit(days[dayInt]).alias("weekday_str")
+    )
 
 
 def save_to_parquet(df, target):
     date_str = NOW.strftime("year=%Y/month=%m/day=%d")
-    path = f"s3a://wt-grepp-lake/optimized/{target}/{date_str}"
-    df.coalesce(50).write.partitionBy("platform").format("parquet").mode("append").save(path)
+    path = f"s3a://wt-grepp-lake/optimized/{target}/{date_str}/platform={PLATFORM}"
+    df.coalesce(50).write.format("parquet").mode("append").save(path)
     print(f"Data successfully optimized to {path}")
-
-
-def convert_weekday(df):
-    return df.withColumn("release_day", 
-                         when(col("weekday_str") == "MONDAY", 0)
-                         .when(col("weekday_str") == "TUESDAY", 1)
-                         .when(col("weekday_str") == "WEDNESDAY", 2)
-                         .when(col("weekday_str") == "THURSDAY", 3)
-                         .when(col("weekday_str") == "FRIDAY", 4)
-                         .when(col("weekday_str") == "SATURDAY", 5)
-                         .when(col("weekday_str") == "SUNDAY", 6)
-                         .otherwise(7)) 
-
-
-def convert_timestamp(df):
-    return df.withColumn("timestamp", from_unixtime(col("start_date") / 1000)) \
-             .withColumn("updated_date", to_date(col("timestamp")))
 
 
 def create_spark_session():
     return SparkSession.builder \
-        .appName("S3 Data Optimizer") \
+        .appName(f"S3 {PLATFORM} Data Optimizer") \
         .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
         .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
@@ -127,19 +68,18 @@ def run():
     dayInt = NOW.weekday()
 
     titles_df = get_titles(spark, date_str, dayInt)
-    titles_df = convert_weekday(titles_df)
-    title_info_df = get_title_info(spark, date_str)
-
-    episodes_df = get_episodes(spark, date_str)
-    episode_likes_df = get_episode_likes(spark, date_str)
-    episode_likes_df = convert_timestamp(episode_likes_df)
-    comments_df = get_comments(spark, date_str)
-
     save_to_parquet(titles_df, "titles")
+
+    title_info_df = get_title_info(spark, date_str)
     save_to_parquet(title_info_df, "title_info")
 
+    episodes_df = get_episodes(spark, date_str)
     save_to_parquet(episodes_df, "episodes")
+    
+    episode_likes_df = get_episode_likes(spark, date_str)
     save_to_parquet(episode_likes_df, "episode_likes")
+
+    comments_df = get_comments(spark, date_str)
     save_to_parquet(comments_df, "comments")
 
 
